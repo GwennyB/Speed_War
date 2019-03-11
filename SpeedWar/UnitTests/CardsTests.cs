220c9e33479c39d795d3791e05ca567badf71884
Gwendolyn Zubatch <gyzubatch@gmail.com>
2019-03-11 09:38:56 -0700

    scaffolded basic project with folders and razor pages

---
+++ SpeedWar/UnitTests/CardsTests.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+//using SpeedWar.Models;
+//using SpeedWar.Data;
+
+namespace UnitTests
+{
+    public class CardsTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
