f99b37e854eb0221713e993ba9fef4d570d91a62
Gwendolyn Zubatch <gyzubatch@gmail.com>
2019-03-11 11:49:05 -0700

    added GetSet tests for Card, DeckCard, Deck, User class properties

--- SpeedWar/UnitTests/DeckCardsTests.cs
+++ SpeedWar/UnitTests/DeckCardsTests.cs
@@ -1,10 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using SpeedWar.Models;
+using Xunit;
 
 namespace UnitTests
 {
-    class DeckCardsTests
+    public class DeckCardsTests
     {
+        [Fact]
+        public void DeckIDGetSet()
+        {
+            DeckCard card = new DeckCard();
+            card.DeckID = 1;
+            Assert.Equal(1, card.DeckID);
+        }
+
+        [Fact]
+        public void CardIDGetSet()
+        {
+            DeckCard card = new DeckCard();
+            card.CardID = 1;
+            Assert.Equal(1, card.CardID);
+        }
+
+        [Fact]
+        public void DeckGetSet()
+        {
+            DeckCard card = new DeckCard();
+            Deck deck = new Deck();
+            card.Deck = deck;
+            Assert.Equal(deck, card.Deck);
+        }
+
+        [Fact]
+        public void CardGetSet()
+        {
+            DeckCard card = new DeckCard();
+            Card refCard = new Card();
+            card.Card = refCard;
+            Assert.Equal(refCard, card.Card);
+        }
     }
 }
--- SpeedWar/UnitTests/UsersTests.cs
+++ SpeedWar/UnitTests/UsersTests.cs
@@ -1,10 +1,27 @@
-﻿using System;
+﻿using SpeedWar.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using Xunit;
 
 namespace UnitTests
 {
-    class UsersTests
+    public class UsersTests
     {
+        [Fact]
+        public void IDGetSet()
+        {
+            User user = new User();
+            user.ID = 1;
+            Assert.Equal(1, user.ID);
+        }
+
+        [Fact]
+        public void NameGetSet()
+        {
+            User user = new User();
+            user.Name = "Test";
+            Assert.Equal("Test", user.Name);
+        }
     }
 }
