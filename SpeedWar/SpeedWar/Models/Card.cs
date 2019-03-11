ea8d91393f6be2bb1d2b81b30b30d94c16e2c201
Gwendolyn Zubatch <gyzubatch@gmail.com>
2019-03-11 10:07:27 -0700

    built all model classes and enums

---
+++ SpeedWar/SpeedWar/Models/Card.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpeedWar.Models
+{
+    public class Card
+    {
+        public int ID { get; set; }
+        public Suit Suit { get; set; }
+        public Rank Rank { get; set; }
+    }
+
+    public enum Suit
+    {
+        hearts,
+        spades,
+        diamonds,
+        clubs
+    }
+
+    public enum Rank
+    {
+        Ace = 1,
+        Two = 2,
+        Three = 3,
+        Four = 4,
+        Five = 5,
+        Six = 6,
+        Seven = 7,
+        Eight = 8,
+        Nine = 9,
+        Ten = 10,
+        Jack = 11,
+        Queen = 12,
+        King = 13
+    }
+}
---
+++ SpeedWar/SpeedWar/Models/Deck.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpeedWar.Models
+{
+    public class Deck
+    {
+        public int ID { get; set; }
+        public int UserID { get; set; }
+        public DeckType DeckType { get; set; }
+
+        // Navigation Properties
+        public User User { get; set; }
+    }
+
+    public enum DeckType
+    {
+        Discard,
+        Play,
+        Collect
+
+    }
+}
