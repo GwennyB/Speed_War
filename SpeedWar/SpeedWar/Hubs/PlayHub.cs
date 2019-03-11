03a1e37b601f265153457d98f20c8d840c59ebc6
Clarice Costello <claricostello@outlook.com>
2019-03-11 10:26:10 -0700

    scaffold Hub

--- SpeedWar/SpeedWar/Hubs/PlayHub.cs
+++ SpeedWar/SpeedWar/Hubs/PlayHub.cs
@@ -1,11 +1,17 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace SpeedWar.Hubs
 {
-    public class PlayHub
+    public class PlayHub : Hub
     {
+        //TO-DO: Scaffold PlayHub
+        public async Task PlayCard(Card card)
+        {
+            await Clients.All.SendAsync("recieve message", card);
+        }
     }
 }
