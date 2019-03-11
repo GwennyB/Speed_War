350100f56592760d69cb59a06621ddb451ac7a75
Gwendolyn Zubatch <gyzubatch@gmail.com>
2019-03-11 11:10:46 -0700

    added interfaces and services (empty)

---
+++ SpeedWar/SpeedWar/Models/Services/DeckCardMgmtSvc.cs
@@ -0,0 +1,12 @@
+﻿using SpeedWar.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpeedWar.Models.Services
+{
+    public class DeckCardMgmtSvc : IDeckCardManager
+    {
+    }
+}
