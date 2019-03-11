83e9725b93e6b08a3fc911699c3abbb811437861
Gwendolyn Zubatch <gyzubatch@gmail.com>
2019-03-11 14:12:53 -0700

    updated launch page with username input and backing code for Get and Post

--- SpeedWar/SpeedWar/Pages/Index.cshtml.cs
+++ SpeedWar/SpeedWar/Pages/Index.cshtml.cs
@@ -4,13 +4,33 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SpeedWar.Models;
+using SpeedWar.Models.Interfaces;
+using SpeedWar.Models.Services;
 
 namespace SpeedWar.Pages
 {
     public class IndexModel : PageModel
     {
+        public IUserManager _user { get; set; }
+        public IndexModel(IUserManager user)
+        {
+            _user = user;
+        }
+
+
+        [BindProperty]
+        public string Username { get; set; }
+        public User Player { get; set; }
+
         public void OnGet()
         {
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            Player = await _user.GetUserAsync(Username);
+            return RedirectToPage("../Play/Index");
         }
     }
 }
