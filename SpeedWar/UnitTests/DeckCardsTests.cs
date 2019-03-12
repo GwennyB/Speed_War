9468c5d3dd3f18fe5c2f6e9a7eeb36d89be8e27d
Gwendolyn Zubatch <gyzubatch@gmail.com>
2019-03-12 10:53:41 -0700

    added test for ResetDecks

--- SpeedWar/UnitTests/DeckCardsTests.cs
+++ SpeedWar/UnitTests/DeckCardsTests.cs
@@ -197,5 +197,42 @@ namespace UnitTests
             }
         }
 
+        [Fact]
+        public async Task CanResetDecks()
+        {
+            DbContextOptions<CardDbContext> options = new DbContextOptionsBuilder<CardDbContext>().UseInMemoryDatabase("ResetDecks").Options;
+
+            using (CardDbContext context = new CardDbContext(options))
+            {
+                DeckCardMgmtSvc svc = new DeckCardMgmtSvc(context);
+                Card card1 = new Card() { ID = 1, Rank = Rank.Ace, Suit = Suit.hearts };
+                Card card2 = new Card() { ID = 2, Rank = Rank.Ace, Suit = Suit.spades };
+                Card card3 = new Card() { ID = 3, Rank = Rank.Ace, Suit = Suit.clubs };
+                Card card4 = new Card() { ID = 4, Rank = Rank.Ace, Suit = Suit.diamonds };
+                await context.Cards.AddAsync(card1);
+                await context.Cards.AddAsync(card2);
+                await context.Cards.AddAsync(card3);
+                await context.Cards.AddAsync(card4);
+                Deck deck1 = new Deck() { ID = 1, UserID = 3, DeckType = DeckType.Collect };
+                Deck deck2 = new Deck() { ID = 2, UserID = 3, DeckType = DeckType.Play };
+                await context.Decks.AddAsync(deck1);
+                await context.Decks.AddAsync(deck2);
+                DeckCard dc1 = new DeckCard() { CardID = 1, DeckID = 1 };
+                DeckCard dc2 = new DeckCard() { CardID = 2, DeckID = 1 };
+                DeckCard dc3 = new DeckCard() { CardID = 3, DeckID = 1 };
+                DeckCard dc4 = new DeckCard() { CardID = 4, DeckID = 1 };
+                await context.DeckCards.AddAsync(dc1);
+                await context.DeckCards.AddAsync(dc2);
+                await context.DeckCards.AddAsync(dc3);
+                await context.DeckCards.AddAsync(dc4);
+                await context.SaveChangesAsync();
+
+                List<DeckCard> queryBefore = await svc.GetDeck(3, DeckType.Collect);
+                await svc.ResetDecks(3);
+                List<DeckCard> queryAfter = await svc.GetDeck(3, DeckType.Play);
+
+                Assert.Equal(queryBefore,queryAfter);
+            }
+        }
     }
 }
