4dcdbe3f37bc42f1c273acc5f08fb504b8b34646
xia LIU <xia.liu2@outlook.com>
2019-03-14 16:01:15 -0700

    updated

--- SpeedWar/SpeedWar/Models/User.cs
+++ SpeedWar/SpeedWar/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,7 @@ namespace SpeedWar.Models
     public class User
     {
         public int ID { get; set; }
+        [Required(ErrorMessage = "Please provide a name.")]
         public string Name { get; set; }
         public bool PlayerTurn { get; set; }
         public int FirstCard { get; set; }
